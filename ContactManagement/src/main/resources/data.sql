63c55c6e671843bd7fc9a6195b9ca89a29e4fbe9
patilabhieet59 <53978562+patilabhieet59@users.noreply.github.com>
2019-12-02 10:37:26 +0530

    Update data.sql

--- ContactManagement/src/main/resources/data.sql
+++ ContactManagement/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO CONTACTS (id,first_name, last_name, email,phone_number,active) VALUES
-  (1,'Lokesh', 'Gupta', 'abc@gmail.com','12345','T'),
+  (1,'Abhijeet', 'Patil', 'abc@gmail.com','12345','T'),
   (2,'Deja', 'Vu', 'xyz@email.com','4567','T'),
   (3,'Caption', 'America', 'cap@marvel.com','56789','T');
